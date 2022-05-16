2c67c644bcbbbc6f95e8c468702b25f0d3227cae
vnikoyan <vnikoyan@gmail.com>
2022-05-16 14:31:55 -0400

    v1

--- 13.05.py
+++ 13.05.py
@@ -1,6 +1,23 @@
-mylist = [21, -9, 15, 2116, -71, 33]
-number = int(input("enter tiv"))
-list = []
-for item in mylist:
-    list.append(abs(item-number))
-print(list.index(min(list)))
+'''
+def func(x, y):
+    if x>y:
+        print(x)
+    else:
+        print(y)
+x = int(input("enter number 1 ------"))
+
+y = int(input("enter number 1 ------"))
+func(x,y)
+'''
+num = str(input())
+
+
+for i in range(0 , len(num)):
+    if num[i] == "+":
+        print(int(num[0 : i])+int(num[(i+1) : len(num)]))
+    elif num[i] == "-" and i == 0:
+        print(-int(num[0 : i])-int(num[(i+1) : len(num)]))
+    elif num[i] == "*":
+        print(int(num[0 : i])*int(num[(i+1) : len(num)]))
+    elif num[i] == "/":
+        print(int(num[0 : i])/int(num[(i+1) : len(num)]))
--- 16.05(functions).py
+++ 16.05(functions).py
@@ -1,4 +1,6 @@
+'''
 def func()
     x=5
     return x
 print(func())
+'''
