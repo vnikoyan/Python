5f16876900a9de91ad4f40632d6d055da311dfb9
vnikoyan <vnikoyan@gmail.com>
2022-05-18 11:57:41 -0400

    final

--- 18.05.py
+++ 18.05.py
@@ -1,7 +1,7 @@
 
 def func(tiv):
     type = "parz"
-    for i in range(2, int(tiv/2)):
+    for i in range(2, int(tiv/2)+1):
         if tiv % i == 0:
             type = "bard"
             break
@@ -10,4 +10,8 @@ def func(tiv):
     return type
 
 
-print(func(int(input())))
+n = int(input())
+for tiv in range(2, n):
+    res = func(tiv)
+    if res == "parz":
+        print(tiv)
